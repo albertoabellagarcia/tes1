250a32d546fc8565003fe5ddfd6444c0f5c817e0
Alberto Abella <59435267+albertoabellagarcia@users.noreply.github.com>
2020-04-07 01:05:36 +0200

    test

---
+++ testing_functions.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+    
+def substract(x,y):
+    return x-y
+    
+def mult(x,y):
+    return x*y
+
+def divide(x,y):
+    if y == 0:
+        raise ValueError("cannot divide by zero")
+    else: 
+        return x+y
+
+ 
